357fcc087aaa3fa2cb0f32daaf922f2950279400
Evie Goodman <evelyng@uoregon.edu>
2023-03-13 14:00:00 -0700

    Fianl d3 code

--- design_challenge/main.js
+++ design_challenge/main.js
@@ -4,6 +4,7 @@ function onCategoryChanged() {
     // Get current value of select element
     var filterKey = select.options[select.selectedIndex].value;
     // Update chart with the selected category of letters
+    console.log(filterKey)
     updateChart(filterKey);
 }
 
@@ -19,28 +20,31 @@ var padding = {t: 10, r: 30, b: 30, l: 60};
 var chartWidth = svgWidth - padding.l - padding.r;
 var chartHeight = svgHeight - padding.t - padding.b;
 
-var parseDate = d3.timeParse('%b %Y');
+var parseDate = d3.timeParse("%m %d %Y");
 var dateDomain = [new Date(2014, 7), new Date(2015, 6)];
 
 // Create a group element for appending chart elements
 var chartG = svg.append('g')
 .attr('transform', 'translate('+[padding.l, padding.t]+')');
 
+var selectOptions = []
+
 d3.csv('KSEA.csv').then(function(dataset) {
     dataset.forEach(function(d) {
-        d.date = parseDate(d.date);
+        d.date = new Date(d.date);
     });
 
     data = dataset;
 
-    let max = d3.max(dataset, function(d) { return +d.actual_mean_temp; })
+    // let max = d3.max(dataset, function(d) { return +d.actual_mean_temp; })
+    // console.log(max)
 
     xScale = d3.scaleTime()
-    .domain(dateDomain)
+    .domain(d3.extent(data, function(d) { return d.date; }))
     .range([ 0, chartWidth ]);
 
     yScale = d3.scaleLinear()
-    .domain([0, max])
+    .domain([0, 100])
     .range([ chartHeight, 0 ]);
 
     // x axis
@@ -51,47 +55,58 @@ d3.csv('KSEA.csv').then(function(dataset) {
     chartG.append("g")
     .call(d3.axisLeft(yScale));
 
+    line = chartG
+    .append('g')
+    .append("path")
+    
     // Update the chart
-    updateChart('actual_mean_temp', xScale, yScale);
+    updateChart('actual_mean_temp');
 });
 
 
-function updateChart(filterKey, xScale, yScale) {
+function updateChart(filterKey) {
     // update dataset based on filterKey
     // var filteredData = data.filter(function(d) {
     //     return d.filterKey;
     // });
+    var dataFilter = data.map(function(d){return {date: d.date, value:d[filterKey]} })
     console.log(filterKey)
+    console.log(data)
 
     let lineInterpolator = d3.line()
+    // .x(function(d) { return d.date })
+    // .y(function(d) { return d[filterKey] });
     .x(d => xScale(d.date))
-    .y(d => yScale(d.filterKey));
+    .y(d => yScale(d[filterKey]))
 
-    // // Set the gradient
-    // chartG.append("linearGradient")
-    // .attr("id", "line-gradient")
-    // .attr("gradientUnits", "userSpaceOnUse")
-    // .attr("x1", 0)
-    // .attr("y1", y(0))
-    // .attr("x2", 0)
-    // .attr("y2", y(max))
-    // .selectAll("stop")
-    //     .dataset([
-    //     {offset: "0%", color: "blue"},
-    //     {offset: "100%", color: "red"}
-    //     ])
-    // .enter().append("stop")
-    //     .attr("offset", function(d) { return d.offset; })
-    //     .attr("stop-color", function(d) { return d.color; });
+    let max = d3.max(data, function(d) { return d[filterKey]; })
+
+    // Set the gradient
+    chartG.append("linearGradient")
+    .attr("id", "line-gradient")
+    .attr("gradientUnits", "userSpaceOnUse")
+    .attr("x1", 0)
+    .attr("y1", yScale(0))
+    .attr("x2", 0)
+    .attr("y2", yScale(max))
+    .selectAll("stop")
+    .data([
+    {offset: "0%", color: "blue"},
+    {offset: "100%", color: "red"}
+    ])
+    .enter()
+    .append("stop")
+        .attr("offset", function(d) { return d.offset; })
+        .attr("stop-color", function(d) { return d.color; });
   
     // Add the line
-    chartG.selectAll(".line-plot")
-    .data(data)
-    .enter()
-    .append('path')
-    .attr('class', ".line-plot")
+    line
+    .datum(data)
+    .transition()
+    .duration(1000)
     .attr('d', lineInterpolator)
-    .attr("stroke", "black") // "url(#line-gradient)" 
+    .attr("stroke", "url(#line-gradient)" ) // "url(#line-gradient)" 
+    .style("fill", "none")
     .attr("stroke-width", 2)
 }
 // Remember code outside of the dataset callback function will run before the dataset loads
